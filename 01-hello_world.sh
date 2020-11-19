93af77da7a847cae281db97a06a8b2d2ff54b9e4
Stéphanie <e.svoyer2@etu.cvm.qc.ca>
2020-11-19 15:32:58 -0500

    echo-01 termine

--- 01-hello_world.sh
+++ 01-hello_world.sh
@@ -1,2 +1,20 @@
 #!/bin/bash
 
+# cecu est un commentaire
+
+echo "salut Antoine"
+#ou
+echo salut Antoine
+#ou
+echo 'salut Antoine'
+
+echo -n "Vive "
+echo "Linus"
+# va écrire vive Linus sans saut de ligne (a cause du -n)
+
+echo -e "1\n2\n3\n4" #\n pour une siate deli ne et -e interprète les métacaratères
+
+read -p "My name is?" nom
+#read = cin, -p "mon ,message" >> variable qui recoit la valeur, ici nom
+
+echo "tchica tchi ca thcai" $nom
---
+++ script.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+#COMMENTAIRE
+
+echo salut!
